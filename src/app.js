1235627b0564a7f8a0d75bd1e7bc583537a2e9a5
Matheus <matheusvmja@gmail.com>
2023-07-30 23:25:18 -0300

    Feat: setup done

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express"
+import cors from "cors"
+import router from "./routes/index.routes"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(router)
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`servidor rodando na porta ${port}`))
