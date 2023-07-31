3fce40a3f48930e5627b742dcbf394172169d0c0
Matheus <matheusvmja@gmail.com>
2023-07-31 09:51:24 -0300

    Feat: PUT customers done

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -26,8 +26,21 @@ export async function createCustomer(req, res) {
 
     try {
         await db.query(`INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4);`, [name, phone, cpf, birthday])
-        
+
         res.sendStatus(201)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+export async function updateCustomer(req, res) {
+    const { id } = req.params
+    const { name, phone, cpf, birthday } = req.body
+
+    try {
+        await db.query(`UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE  id = $5;`, [name, phone, cpf, birthday, id])
+
+        res.sendStatus(200)
     } catch (err) {
         res.status(500).send(err.message)
     }
--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { getCustomers, getCustomerById, createCustomer } from "../controllers/customers.controller.js"
+import { getCustomers, getCustomerById, createCustomer, updateCustomer } from "../controllers/customers.controller.js"
 import validateSchema from "../middlewares/validateSchema.middleware.js"
 import { customerSchema } from "../schemas/customer.schemas.js"
 import { validateCreateCustomer } from "../middlewares/customers.middleware.js"
@@ -9,5 +9,6 @@ const customerRouter = Router()
 customerRouter.get("/customers", getCustomers)
 customerRouter.get("/customers/:id", getCustomerById)
 customerRouter.post("/customers", validateSchema(customerSchema), validateCreateCustomer, createCustomer)
+customerRouter.put("/customers/:id", validateSchema(customerSchema), validateCreateCustomer, updateCustomer)
 
 export default customerRouter
