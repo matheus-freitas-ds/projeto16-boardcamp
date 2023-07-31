e22ae58f44f04f15287e562caae646a0085aadb3
Matheus <matheusvmja@gmail.com>
2023-07-31 17:00:02 -0300

    Chore: code adjusted for deploy on render

--- src/database/database.js
+++ src/database/database.js
@@ -9,4 +9,6 @@ const configDatabase = {
   connectionString: process.env.DATABASE_URL,
 }
 
+if (process.env.NODE_ENV === "production") configDatabase.ssl = true
+
 export const db = new Pool(configDatabase)
