293c03a5ba0ad348dc00da04e5484eb32e32761d
Matheus <matheusvmja@gmail.com>
2023-07-31 04:33:10 -0300

    Fix: typing error on customers middleware

--- src/middlewares/customers.middleware.js
+++ src/middlewares/customers.middleware.js
@@ -1,10 +1,10 @@
 import { db } from "../database/database.js"
 
 export async function validateCreateCustomer(req, res, next) {
-    const { name, phone, cpf, birthday } = req.body
+    const { cpf } = req.body
 
     try {
-        const cpfList = await db.query(`SELECT * FROM customers WHERE cpf = $3;`, [cpf])
+        const cpfList = await db.query(`SELECT * FROM customers WHERE cpf = $1;`, [cpf])
 
         if (cpfList.rowCount !== 0) return res.sendStatus(409)
 
